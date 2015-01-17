0807ae85e063f636021b7016e81592903e61c429
jifeon <balakirev.andrey@gmail.com>
2015-01-17 22:20:43 +0300

    complete example

--- examples/list/ListView.js
+++ examples/list/ListView.js
@@ -1,11 +1,34 @@
-define(['lib/Backbone.View.Elements'], function (ElementsView) {
+define(['jquery', 'lib/Backbone.View.Elements'], function ($, ElementsView) {
     /**
      * @class ListView
      * @extends ElementsView
      */
     var ListView = ElementsView.extend(/**@lends ListView*/{
-        events: function () {
+        _selectors: function () {
+            var removingButton = '.list__remove-item-button';
+            return {
+                itemByNumber: '.list__item[data-item-number=%s]',
+                removingButton: removingButton,
+                removingButtonByNumber: removingButton + '[data-item-number=%s]'
+            };
+        },
 
+        events: function () {
+            var events = {};
+            events['click ' + this._selector('removingButton')] = this._onRemovingButtonClick;
+            return events;
+        },
+
+        _onRemovingButtonClick: function (e) {
+            var itemNumber = $(e.currentTarget).data('itemNumber');
+            this._removeItem(itemNumber);
+        },
+
+        _removeItem: function (itemNumber) {
+            this._elem('itemByNumber', itemNumber).remove();
+            this._elem('removingButtonByNumber', itemNumber).remove();
         }
     });
+
+    return ListView;
 });
